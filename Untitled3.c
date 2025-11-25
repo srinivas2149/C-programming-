1926cb5f2cf0aee78bf9b326fab9bcbce7513405
srinivas2149 <m.srinivas2149@gmail.com>
2025-11-25 11:02:27 +0530

    Create Untitled3.c

---
+++ Untitled3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+
+    printf("Enter a integer: ");
+    scanf("%d", &n);
+
+    if (n < 0)
+    {
+    printf("You entered %d.\n",n);
+    }
+
+
+    return 0;
+}
