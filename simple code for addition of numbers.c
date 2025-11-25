b55227431b53ef44b5b09982d9dbf8b451fb2142
srinivas2149 <m.srinivas2149@gmail.com>
2025-11-25 11:48:54 +0530

    Create simple code for addition of numbers.c

---
+++ simple code for addition of numbers.c
@@ -0,0 +1,21 @@
+
+#include <stdio.h>
+
+int main() {
+    int a, b, result;
+
+    // Ask the user for two numbers
+    printf("Enter first number: ");
+    scanf("%d", &a);
+
+    printf("Enter second number: ");
+    scanf("%d", &b);
+
+    // Add a and b
+    result = a + b;
+
+    // Show the result
+    printf("Result of addition: %d\n", result);
+
+    return 0;
+}
