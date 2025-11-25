b9f5047b5866f07374a9a18db9000005d1fa991c
srinivas2149 <m.srinivas2149@gmail.com>
2025-11-25 11:42:40 +0530

    Create count no elements in array.c

---
+++ count no elements in array.c
@@ -0,0 +1,14 @@
+
+// Program: Counting elements in an array
+#include <stdio.h>
+
+int main() {
+    int arr[] = {10, 20, 30, 40, 50};
+
+    // Calculate number of elements
+    int count = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Total number of elements in the array = %d\n", count);
+
+    return 0;
+}
