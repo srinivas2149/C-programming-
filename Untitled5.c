6aaf38b7cb2d3b7e7b1249a9eddd15d25380b001
srinivas2149 <m.srinivas2149@gmail.com>
2025-11-25 11:03:50 +0530

    Create Untitled5.c

---
+++ Untitled5.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int num = 1; // Start from 1
+
+    while (num <= 5) { // Continue while num is less than or equal to 5
+        printf("%d\n", num); // Print current number
+        num++; // Increment number by 1
+    }
+
+    return 0;
+}
